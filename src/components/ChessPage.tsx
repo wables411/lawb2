663900ce85c2478732d5e80a0c21c19397f47035
Runitupronnie <runitupronnie@gmail.com>
2025-07-16 12:15:48 -0500

    Fix chess piece gallery scrolling and title bar close button functionality

--- src/components/ChessPage.tsx
+++ src/components/ChessPage.tsx
@@ -7,13 +7,18 @@ import './ChessPage.css';
 const ChessPage: React.FC = () => {
   const [gameMode, setGameMode] = useState<'singleplayer' | 'multiplayer'>('singleplayer');
 
+  const handleClose = () => {
+    // Navigate back to main site
+    window.location.href = '/';
+  };
+
   return (
     <div className="chess-page">
       <div className="chess-content">
         {gameMode === 'singleplayer' ? (
-          <ChessGame onClose={() => {}} />
+          <ChessGame onClose={handleClose} />
         ) : (
-          <ChessMultiplayer onClose={() => {}} onMinimize={() => {}} fullscreen={false} />
+          <ChessMultiplayer onClose={handleClose} onMinimize={() => {}} fullscreen={false} />
         )}
       </div>
     </div>
