839f3a1813406cff9bae2e4708f2c5cbd39d8436
wables411 <wables411@users.noreply.github.com>
2025-07-17 12:41:01 +0000

    deploy: fe6355e8c24523be2c4a98dd6efbb8a42c8852d0

---
+++ assets/transactions-B_JvJyxo.js
@@ -0,0 +1,16 @@
+import{i as f,a as m,x as a}from"./index-BGEYvgQb.js";import{c as p}from"./if-defined-CStlVVb5.js";import"./index-f1RPDneH.js";import"./index-DByP300r.js";import"./index-CVIJP7oJ.js";import"./index-DB70faFF.js";import"./index-D7yyW9aE.js";import"./index-Dsi9mOgo.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var c=o.length-1;c>=0;c--)(l=o[c])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let s=class extends m{render(){return a`
+      <wui-flex flexDirection="column" .padding=${["0","m","m","m"]} gap="s">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};s.styles=d;s=u([p("w3m-transactions-view")],s);export{s as W3mTransactionsView};
