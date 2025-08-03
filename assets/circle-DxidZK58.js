d14af27b6f380fbac8c5416d664b772df3181dce
wables411 <wables411@users.noreply.github.com>
2025-08-03 17:42:15 +0000

    deploy: 560f185be77df11c24228fb3a20263250871f792

--- assets/circle-DxidZK58.js
+++
@@ -1,4 +0,0 @@
-import{b as i}from"./appkit-vendor-C7zlPPaF.js";import"./wagmi-vendor-dPYIeXYB.js";import"./react-vendor-CFG7hIzY.js";const o=i`<svg width="4" height="4" viewBox="0 0 4 4" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="2" cy="2" r="2" fill="white" fill-opacity="0.2"/>
-</svg>
-`;export{o as circleSvg};
