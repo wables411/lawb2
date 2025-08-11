860a4a1c57dc7bb311e2ef4c6f2e2bdc5c7e70af
wables411 <wables411@users.noreply.github.com>
2025-08-11 02:07:55 +0000

    deploy: a5d1c2daec869c4c72fe5558df293eb488ff6dc5

---
+++ assets/circle-DwQQTdTy.js
@@ -0,0 +1,4 @@
+import{b as i}from"./index-Dfiw19v5.js";import"./wagmi-vendor-D-kpDhCQ.js";import"./react-vendor-ZyuiJZO_.js";import"./chess-vendor-JTxzwGi1.js";import"./ui-vendor-BgPmeekb.js";const o=i`<svg width="4" height="4" viewBox="0 0 4 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="2" cy="2" r="2" fill="white" fill-opacity="0.2"/>
+</svg>
+`;export{o as circleSvg};
