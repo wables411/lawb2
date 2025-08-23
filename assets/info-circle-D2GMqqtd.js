8dd2a95eafc0289b8b1b2c9a102acfcc2a8fb093
wables411 <wables411@users.noreply.github.com>
2025-08-23 18:55:07 +0000

    deploy: a706cd8d55592f61d2c7894a54793839f9178006

---
+++ assets/info-circle-D2GMqqtd.js
@@ -0,0 +1,12 @@
+import{b as o}from"./index-DzfBTSo3.js";import"./wagmi-vendor-D3Lspqub.js";import"./react-vendor-ZyuiJZO_.js";import"./chess-vendor-JTxzwGi1.js";import"./ui-vendor-BgPmeekb.js";const r=o`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    d="M6 10.49a1 1 0 1 0 2 0v-2a1 1 0 0 0-2 0v2ZM7 4.49a1 1 0 1 0 0 2 1 1 0 0 0 0-2Z"
+  />
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M7 14.99a7 7 0 1 0 0-14 7 7 0 0 0 0 14Zm5-7a5 5 0 1 1-10 0 5 5 0 0 1 10 0Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{r as infoCircleSvg};
