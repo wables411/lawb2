8f1590abeaea2a7651ee6242391948200e4ebc6d
wables411 <wables411@users.noreply.github.com>
2025-08-23 22:28:02 +0000

    deploy: d31f5c1b1eae3d2c37c5d71bfcd689653f0abba9

--- assets/circle-Ca10lQSP.js
+++
@@ -1,4 +0,0 @@
-import{b as i}from"./index-CefgnnXl.js";import"./wagmi-vendor-Dfx1X9pT.js";import"./react-vendor-ZyuiJZO_.js";import"./chess-vendor-JTxzwGi1.js";import"./ui-vendor-BgPmeekb.js";const o=i`<svg width="4" height="4" viewBox="0 0 4 4" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="2" cy="2" r="2" fill="white" fill-opacity="0.2"/>
-</svg>
-`;export{o as circleSvg};
