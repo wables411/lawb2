d4dcc8328e11e5208d337e2c4e9e2e45a594f251
wables411 <wables411@users.noreply.github.com>
2025-08-23 23:02:12 +0000

    deploy: 0f4dce5b53c42e11b36eaeefb5497e547155f6eb

---
+++ assets/arrow-bottom-COvAkoSt.js
@@ -0,0 +1,8 @@
+import{b as o}from"./index-Csl6fNw7.js";import"./wagmi-vendor-Dfx1X9pT.js";import"./react-vendor-ZyuiJZO_.js";import"./chess-vendor-JTxzwGi1.js";import"./ui-vendor-BgPmeekb.js";const e=o`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M7 1.99a1 1 0 0 1 1 1v7.58l2.46-2.46a1 1 0 0 1 1.41 1.42L7.7 13.69a1 1 0 0 1-1.41 0L2.12 9.53A1 1 0 0 1 3.54 8.1L6 10.57V3a1 1 0 0 1 1-1Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{e as arrowBottomSvg};
