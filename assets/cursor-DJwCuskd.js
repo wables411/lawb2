3e0d842ab67f035d980470408bce974cf4144609
wables411 <wables411@users.noreply.github.com>
2025-08-24 02:49:23 +0000

    deploy: 5cd6f0407d73e894f158445d622edf7a3404fe46

---
+++ assets/cursor-DJwCuskd.js
@@ -0,0 +1,3 @@
+import{b as o}from"./index-thYaA2cg.js";import"./wagmi-vendor-B_qL9Hk_.js";import"./react-vendor-ZyuiJZO_.js";import"./chess-vendor-JTxzwGi1.js";import"./ui-vendor-BgPmeekb.js";const r=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{r as cursorSvg};
