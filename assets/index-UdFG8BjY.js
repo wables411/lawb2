158c33a8f82e941f86cfb29a3a8ac709d9c22524
wables411 <wables411@users.noreply.github.com>
2025-08-25 00:08:17 +0000

    deploy: be543ea7c17a00e5aac7ca1f227b33c6349de3af

---
+++ assets/index-UdFG8BjY.js
@@ -0,0 +1,138 @@
+import{i as t,r as e,e as i,n as o,c as n,d as r,x as s,O as a,U as l,a0 as u,R as p,u as c,k as h,S as d}from"./index-BSOfQUI5.js";const g=t`
+  :host {
+    position: relative;
+    display: inline-block;
+  }
+
+  input {
+    width: 50px;
+    height: 50px;
+    background: var(--wui-color-gray-glass-010);
+    border-radius: var(--wui-border-radius-xs);
+    border: 1px solid var(--wui-color-gray-glass-005);
+    font-family: var(--wui-font-family);
+    font-size: var(--wui-font-size-large);
+    font-weight: var(--wui-font-weight-regular);
+    letter-spacing: var(--wui-letter-spacing-large);
+    text-align: center;
+    color: var(--wui-color-fg-100);
+    caret-color: var(--wui-color-accent-100);
+    transition:
+      background-color var(--wui-ease-inout-power-1) var(--wui-duration-md),
+      border-color var(--wui-ease-inout-power-1) var(--wui-duration-md),
+      box-shadow var(--wui-ease-inout-power-1) var(--wui-duration-md);
+    will-change: background-color, border-color, box-shadow;
+    box-sizing: border-box;
+    -webkit-appearance: none;
+    -moz-appearance: textfield;
+    padding: 0px;
+  }
+
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  input[type='number'] {
+    -moz-appearance: textfield;
+  }
+
+  input:disabled {
+    cursor: not-allowed;
+    border: 1px solid var(--wui-color-gray-glass-010);
+    background: var(--wui-color-gray-glass-005);
+  }
+
+  input:focus:enabled {
+    background-color: var(--wui-color-gray-glass-015);
+    border: 1px solid var(--wui-color-accent-100);
+    -webkit-box-shadow: 0px 0px 0px 4px var(--wui-box-shadow-blue);
+    -moz-box-shadow: 0px 0px 0px 4px var(--wui-box-shadow-blue);
+    box-shadow: 0px 0px 0px 4px var(--wui-box-shadow-blue);
+  }
+
+  @media (hover: hover) and (pointer: fine) {
+    input:hover:enabled {
+      background-color: var(--wui-color-gray-glass-015);
+    }
+  }
+`;var m=function(t,e,i,o){var n,r=arguments.length,s=r<3?e:null===o?o=Object.getOwnPropertyDescriptor(e,i):o;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)s=Reflect.decorate(t,e,i,o);else for(var a=t.length-1;a>=0;a--)(n=t[a])&&(s=(r<3?n(s):r>3?n(e,i,s):n(e,i))||s);return r>3&&s&&Object.defineProperty(e,i,s),s};let w=class extends r{constructor(){super(...arguments),this.disabled=!1,this.value=""}render(){return s`<input
+      type="number"
+      maxlength="1"
+      inputmode="numeric"
+      autofocus
+      ?disabled=${this.disabled}
+      value=${this.value}
+    /> `}};w.styles=[e,i,g],m([o({type:Boolean})],w.prototype,"disabled",void 0),m([o({type:String})],w.prototype,"value",void 0),w=m([n("wui-input-numeric")],w);const f=t`
+  :host {
+    position: relative;
+    display: block;
+  }
+`;var v=function(t,e,i,o){var n,r=arguments.length,s=r<3?e:null===o?o=Object.getOwnPropertyDescriptor(e,i):o;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)s=Reflect.decorate(t,e,i,o);else for(var a=t.length-1;a>=0;a--)(n=t[a])&&(s=(r<3?n(s):r>3?n(e,i,s):n(e,i))||s);return r>3&&s&&Object.defineProperty(e,i,s),s};let b=class extends r{constructor(){super(...arguments),this.length=6,this.otp="",this.values=Array.from({length:this.length}).map(()=>""),this.numerics=[],this.shouldInputBeEnabled=t=>this.values.slice(0,t).every(t=>""!==t),this.handleKeyDown=(t,e)=>{const i=t.target,o=this.getInputElement(i);if(!o)return;["ArrowLeft","ArrowRight","Shift","Delete"].includes(t.key)&&t.preventDefault();const n=o.selectionStart;switch(t.key){case"ArrowLeft":n&&o.setSelectionRange(n+1,n+1),this.focusInputField("prev",e);break;case"ArrowRight":case"Shift":this.focusInputField("next",e);break;case"Delete":case"Backspace":""===o.value?this.focusInputField("prev",e):this.updateInput(o,e,"")}},this.focusInputField=(t,e)=>{if("next"===t){const t=e+1;if(!this.shouldInputBeEnabled(t))return;const i=this.numerics[t<this.length?t:e],o=i?this.getInputElement(i):void 0;o&&(o.disabled=!1,o.focus())}if("prev"===t){const t=e-1,i=this.numerics[t>-1?t:e],o=i?this.getInputElement(i):void 0;o&&o.focus()}}}firstUpdated(){var t,e;this.otp&&(this.values=this.otp.split(""));const i=null==(t=this.shadowRoot)?void 0:t.querySelectorAll("wui-input-numeric");i&&(this.numerics=Array.from(i)),null==(e=this.numerics[0])||e.focus()}render(){return s`
+      <wui-flex gap="xxs" data-testid="wui-otp-input">
+        ${Array.from({length:this.length}).map((t,e)=>s`
+            <wui-input-numeric
+              @input=${t=>this.handleInput(t,e)}
+              @click=${t=>this.selectInput(t)}
+              @keydown=${t=>this.handleKeyDown(t,e)}
+              .disabled=${!this.shouldInputBeEnabled(e)}
+              .value=${this.values[e]||""}
+            >
+            </wui-input-numeric>
+          `)}
+      </wui-flex>
+    `}updateInput(t,e,i){const o=this.numerics[e],n=t||(o?this.getInputElement(o):void 0);n&&(n.value=i,this.values=this.values.map((t,o)=>o===e?i:t))}selectInput(t){const e=t.target;if(e){const t=this.getInputElement(e);null==t||t.select()}}handleInput(t,e){const i=t.target,o=this.getInputElement(i);if(o){const i=o.value;if("insertFromPaste"===t.inputType)this.handlePaste(o,i,e);else{l.isNumber(i)&&t.data?(this.updateInput(o,e,t.data),this.focusInputField("next",e)):this.updateInput(o,e,"")}}this.dispatchInputChangeEvent()}handlePaste(t,e,i){const o=e[0];if(o&&l.isNumber(o)){this.updateInput(t,i,o);const n=e.substring(1);if(i+1<this.length&&n.length){const t=this.numerics[i+1],e=t?this.getInputElement(t):void 0;e&&this.handlePaste(e,n,i+1)}else this.focusInputField("next",i)}else this.updateInput(t,i,"")}getInputElement(t){var e;return(null==(e=t.shadowRoot)?void 0:e.querySelector("input"))?t.shadowRoot.querySelector("input"):null}dispatchInputChangeEvent(){const t=this.values.join("");this.dispatchEvent(new CustomEvent("inputChange",{detail:t,bubbles:!0,composed:!0}))}};b.styles=[e,f],v([o({type:Number})],b.prototype,"length",void 0),v([o({type:String})],b.prototype,"otp",void 0),v([a()],b.prototype,"values",void 0),b=v([n("wui-otp")],b);const x=t`
+  wui-loading-spinner {
+    margin: 9px auto;
+  }
+
+  .email-display,
+  .email-display wui-text {
+    max-width: 100%;
+  }
+`;var y,I=function(t,e,i,o){var n,r=arguments.length,s=r<3?e:null===o?o=Object.getOwnPropertyDescriptor(e,i):o;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)s=Reflect.decorate(t,e,i,o);else for(var a=t.length-1;a>=0;a--)(n=t[a])&&(s=(r<3?n(s):r>3?n(e,i,s):n(e,i))||s);return r>3&&s&&Object.defineProperty(e,i,s),s};let T=y=class extends r{firstUpdated(){this.startOTPTimeout()}disconnectedCallback(){clearTimeout(this.OTPTimeout)}constructor(){var t;super(),this.loading=!1,this.timeoutTimeLeft=u.getTimeToNextEmailLogin(),this.error="",this.otp="",this.email=null==(t=p.state.data)?void 0:t.email,this.authConnector=c.getAuthConnector()}render(){if(!this.email)throw new Error("w3m-email-otp-widget: No email provided");const t=Boolean(this.timeoutTimeLeft),e=this.getFooterLabels(t);return s`
+      <wui-flex
+        flexDirection="column"
+        alignItems="center"
+        .padding=${["l","0","l","0"]}
+        gap="l"
+      >
+        <wui-flex
+          class="email-display"
+          flexDirection="column"
+          alignItems="center"
+          .padding=${["0","xl","0","xl"]}
+        >
+          <wui-text variant="paragraph-400" color="fg-100" align="center">
+            Enter the code we sent to
+          </wui-text>
+          <wui-text variant="paragraph-500" color="fg-100" lineClamp="1" align="center">
+            ${this.email}
+          </wui-text>
+        </wui-flex>
+
+        <wui-text variant="small-400" color="fg-200">The code expires in 20 minutes</wui-text>
+
+        ${this.loading?s`<wui-loading-spinner size="xl" color="accent-100"></wui-loading-spinner>`:s` <wui-flex flexDirection="column" alignItems="center" gap="xs">
+              <wui-otp
+                dissabled
+                length="6"
+                @inputChange=${this.onOtpInputChange.bind(this)}
+                .otp=${this.otp}
+              ></wui-otp>
+              ${this.error?s`
+                    <wui-text variant="small-400" align="center" color="error-100">
+                      ${this.error}. Try Again
+                    </wui-text>
+                  `:null}
+            </wui-flex>`}
+
+        <wui-flex alignItems="center" gap="xs">
+          <wui-text variant="small-400" color="fg-200">${e.title}</wui-text>
+          <wui-link @click=${this.onResendCode.bind(this)} .disabled=${t}>
+            ${e.action}
+          </wui-link>
+        </wui-flex>
+      </wui-flex>
+    `}startOTPTimeout(){this.timeoutTimeLeft=u.getTimeToNextEmailLogin(),this.OTPTimeout=setInterval(()=>{this.timeoutTimeLeft>0?this.timeoutTimeLeft=u.getTimeToNextEmailLogin():clearInterval(this.OTPTimeout)},1e3)}async onOtpInputChange(t){var e;try{this.loading||(this.otp=t.detail,this.shouldSubmitOnOtpChange()&&(this.loading=!0,await(null==(e=this.onOtpSubmit)?void 0:e.call(this,this.otp))))}catch(i){this.error=h.parseError(i),this.loading=!1}}async onResendCode(){try{if(this.onOtpResend){if(!this.loading&&!this.timeoutTimeLeft){this.error="",this.otp="";if(!c.getAuthConnector()||!this.email)throw new Error("w3m-email-otp-widget: Unable to resend email");this.loading=!0,await this.onOtpResend(this.email),this.startOTPTimeout(),d.showSuccess("Code email resent")}}else this.onStartOver&&this.onStartOver()}catch(t){d.showError(t)}finally{this.loading=!1}}getFooterLabels(t){return this.onStartOver?{title:"Something wrong?",action:"Try again "+(t?`in ${this.timeoutTimeLeft}s`:"")}:{title:"Didn't receive it?",action:"Resend "+(t?`in ${this.timeoutTimeLeft}s`:"Code")}}shouldSubmitOnOtpChange(){return this.authConnector&&this.otp.length===y.OTP_LENGTH}};T.OTP_LENGTH=6,T.styles=x,I([a()],T.prototype,"loading",void 0),I([a()],T.prototype,"timeoutTimeLeft",void 0),I([a()],T.prototype,"error",void 0),T=y=I([n("w3m-email-otp-widget")],T);export{T as W};
