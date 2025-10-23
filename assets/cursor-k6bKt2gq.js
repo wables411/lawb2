c4fe419f3328409c465a54e00a0ecb25dbbea6e0
wables411 <wables411@users.noreply.github.com>
2025-10-23 19:56:26 +0000

    deploy: 865b5db66c9ebf709f09cf463f98361c6bb0a8f1

--- assets/cursor-k6bKt2gq.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./index-BJNOQvnE.js";import"./wagmi-vendor-D-kpDhCQ.js";import"./react-vendor-ZyuiJZO_.js";import"./chess-vendor-JTxzwGi1.js";import"./ui-vendor-BgPmeekb.js";const r=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{r as cursorSvg};
