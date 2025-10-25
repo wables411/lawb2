338ee6258722f6ac60b00db134f4de784035993d
wables411 <wables411@users.noreply.github.com>
2025-10-25 18:02:37 +0000

    deploy: 93a3b013f7fb135cff7f74ee14ff245708d51700

---
+++ assets/all-wallets-HgOJ7Ybx.js
@@ -0,0 +1,6 @@
+import{b as o}from"./index-DKH9ieQ0.js";import"./wagmi-vendor-D-kpDhCQ.js";import"./react-vendor-ZyuiJZO_.js";import"./chess-vendor-JTxzwGi1.js";import"./ui-vendor-BgPmeekb.js";const r=o`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{r as allWalletsSvg};
