fe4dd266abc26d289ac5ad1ce0fc328bbfee5c2c
wables411 <wables411@users.noreply.github.com>
2026-02-25 23:50:23 +0000

    deploy: 73734e03cae26d5a14838ef05c36b1ca77a25d11

---
+++ assets/transactions-Ckr7ztjF.js
@@ -0,0 +1,16 @@
+import{a6 as e,a0 as t,a1 as i,$ as r}from"./index-DdUN4dz-.js";import"./wagmi-vendor-DNR7MlZp.js";import"./react-vendor-BlDtUSDV.js";import"./chess-vendor-BKDUb1NN.js";import"./ui-vendor-DXGEkimk.js";const o=e`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;let s=class extends t{render(){return i`
+      <wui-flex flexDirection="column" .padding=${["0","3","3","3"]} gap="3">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};s.styles=o,s=function(e,t,i,r){var o,s=arguments.length,n=s<3?t:null===r?r=Object.getOwnPropertyDescriptor(t,i):r;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)n=Reflect.decorate(e,t,i,r);else for(var l=e.length-1;l>=0;l--)(o=e[l])&&(n=(s<3?o(n):s>3?o(t,i,n):o(t,i))||n);return s>3&&n&&Object.defineProperty(t,i,n),n}([r("w3m-transactions-view")],s);export{s as W3mTransactionsView};
